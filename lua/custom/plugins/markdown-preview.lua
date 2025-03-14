3eb4b83a5e53b671a924876b1de518e7ea89107c
Moritz Rinow <m.rinow@topmotive.eu>
2025-03-14 17:20:33 +0100

    markdown preview

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -3,7 +3,6 @@ return {
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = { 'markdown' },
   build = function()
-    vim.cmd [[Lazy load markdown-preview.nvim]]
     vim.fn['mkdp#util#install']()
   end,
 }
